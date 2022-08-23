3994b4f1408059ec8d370d8ed4a9c517477d4eab
Ricky Hadjirul <hadjirulricky@gmail.com>
2022-08-23 22:04:13 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const userRoute = require('./routes/user')
+const eventRoute = require('./routes/event')
+
+app.use(express.urlencoded({ extended : false }))
+app.use(express.json())
+
+app.use('/api/user', userRoute)
+app.use('/api/event', eventRoute)
+
+app.listen(5000, () => {
+    console.log("Server is listening on port 5000.")
+})
---
+++ routes/event.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+
+router.post('/', (req, res) => {
+
+})
+
+router.post('/:code/join', (req, res) => {
+
+})
+
+router.post('/:code/complete', (req, res) => {
+
+})
