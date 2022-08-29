57943571ecb08aa9cc6acb625efa7e8275761b88
Ricky Hadjirul <hadjirulricky@gmail.com>
2022-08-30 03:02:06 +0800

    Added events constants

---
+++ constants/events.js
@@ -0,0 +1,9 @@
+module.exports = {
+  Event: {
+    EventStarted: "event:started",
+    EventCompleted: "event:completed",
+    EventParticipantCancelled: "event:participant-cancelled",
+    EventParticipantJoin: "event:participant-join",
+    EventParticipantUpdated: "event:participant-updated",
+  },
+};
