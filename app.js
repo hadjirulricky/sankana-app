2ad4cce5fe648a2c652758d1bc40d4eee11e5eb5
Ricky Hadjirul <hadjirulricky@gmail.com>
2022-09-02 21:34:47 +0800

    Updated port

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const eventRoute = require("./routes/event");
 const mongoose = require("mongoose");
 const url = `mongodb+srv://root:WOsk76rQtgzYfUa0@cluster0.bzpen.mongodb.net/sankana`;
 
+const port = process.env.PORT || 5000;
+
 mongoose.connect(url, { useUnifiedTopology: true, useNewUrlParser: true });
 
 const connection = mongoose.connection;
@@ -25,6 +27,6 @@ app.use(express.json());
 app.use("/api/user", userRoute);
 app.use("/api/event", eventRoute);
 
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log("Server is listening on port 5000.");
 });
